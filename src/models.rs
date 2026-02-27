3f71db91c26c9577a8a450d6fdd6bb2d0b42a1ff
Tamir Dingjan <tamir.dingjan@gmail.com>
2026-02-27 22:53:27 +0200

    init stage 1 step 1

---
+++ src/models.rs
@@ -0,0 +1,51 @@
+use std::collections::HashMap;
+
+pub enum Status {
+    Open,
+    InProgress,
+    Resolved,
+    Closed,
+}
+
+pub struct Epic {
+    pub name: String,
+    pub description: String,
+    pub status : Status,
+    pub stories: Vec<i32>,
+}
+
+impl Epic {
+    pub fn new(name: String, description: String) -> Self {
+        // by default the status should be set to open and the stories should be an empty vector
+        Epic {
+            name,
+            description,
+            status: Status::Open,
+            stories: Vec::new(),
+        }
+    }
+}
+
+pub struct Story {
+    pub name: String,
+    pub description: String,
+    pub status: Status,
+}
+
+impl Story {
+    pub fn new(name: String, description: String) -> Self {
+        // by default the status should be set to open
+        Story {
+            name,
+            description,
+            status: Status::Open,
+        }
+    }
+}
+
+pub struct DBState {
+    // This struct represents the entire db state which includes the last_item_id, epics, and stories
+    pub last_item_id: u32,
+    pub epics: HashMap<u32, Epic>,
+    pub stories: HashMap<u32, Story>,
+}
